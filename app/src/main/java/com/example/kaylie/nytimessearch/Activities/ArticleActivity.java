56d9dbddba93bc038ecdc6813dcf8916d536ce5d
Kaylie Lu <kaylielu8@gmail.com>
2016-06-22 00:15:45 -0700

    tried to finish share in actionbar

--- app/src/main/java/com/example/kaylie/nytimessearch/Activities/ArticleActivity.java
+++ app/src/main/java/com/example/kaylie/nytimessearch/Activities/ArticleActivity.java
@@ -1,10 +1,15 @@
 package com.example.kaylie.nytimessearch.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -12,6 +17,8 @@ import android.webkit.WebViewClient;
 import com.example.kaylie.nytimessearch.R;
 
 public class ArticleActivity extends AppCompatActivity {
+
+    private ShareActionProvider miShareAction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,10 +40,37 @@ public class ArticleActivity extends AppCompatActivity {
 
         );
 
+
+
         webView.loadUrl(url);
 
 
     }
 
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate menu resource file.
+        getMenuInflater().inflate(R.menu.menu_search, menu);
+        // Locate MenuItem with ShareActionProvider
+        MenuItem item = menu.findItem(R.id.menu_item_share);
+        // Fetch reference to the share action provider
+        miShareAction = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+        // Return true to display menu
+
+        ShareActionProvider miShare = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+
+        // get reference to WebView
+        WebView wvArticle = (WebView) findViewById(R.id.wvArticle);
+        // pass in the URL currently being used by the WebView
+        shareIntent.putExtra(Intent.EXTRA_TEXT, wvArticle.getUrl());
+
+        miShare.setShareIntent(shareIntent);
+        return super.onCreateOptionsMenu(menu);
+
+    }
 }
 
